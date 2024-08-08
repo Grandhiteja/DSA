14b0e1e225f2c3e19f2146102f02e84b79c9d6d5
Grandhiteja <tejagrandhi98@gmail.com>
2024-08-08 23:03:19 +0530

    ASSIGN1.C

--- ASSIGN1.C
+++ ASSIGN1.C
@@ -1,62 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct Product {
-    char name[100];
-    char id[10];
-    float price;
+struct Friends {
+    char name[50];
+    char pet_name[50];
+    double phone_number;
+    struct type{
+        char type_of_friends[50];
+        char name_of_friends[50];
+        int place_visited;
+    } t;
 };
-
-int main() {
-    int n;
-    printf("Enter the number of products: ");
-    scanf("%d", &n);
-
-    struct Product* products = (struct Product*)malloc(n * sizeof(struct Product));
-
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter details of product %d:\n", i + 1);
-        printf("Product Name: ");
-        scanf("%s", (products + i)->name);
-        printf("Product ID: ");
-        scanf("%s", (products + i)->id);
-        printf("Price: ");
-        scanf("%f", &(products + i)->price);
+int main()
+{
+    int i,n;
+    printf("ENTER THE NUMBER OF FRIENDS TO ADD:");
+    scanf("%d",&n);
+    struct Friends f[n];
+    for(i=0;i<n;i++){
+        printf("ENTER FRIEND NAME: ");
+        scanf("%s",&f[i].name);
+        printf("ENTER PET NAME: ");
+        scanf("%s",&f[i].pet_name);
+        printf("ENTER PHONE NUMBER: ");
+        scanf("%lf", &f[i].phone_number);
+        printf("ENTER TYPE OF FRIENDS: ");
+        scanf("%s",&f[i].t.type_of_friends);
+        printf("ENTER NAME OF COMMON FRIENDS: ");
+        scanf("%s",&f[i].t.name_of_friends);
+        printf("ENTER NUMBER OF PLACE VISITED TOGETHER: ");
+        scanf("%d",&f[i].t.place_visited);
     }
-
-    printf("\nProduct Details:\n");
-    for (int i = 0; i < n; i++) {
-        printf("Product Name: %s, Product ID: %s, Price: %.2f\n",
-               (products + i)->name, (products + i)->id, (products + i)->price);
+    printf("S.NO.  \t");
+    printf("FRIENDS LIST NAME \t");
+    printf("PET NAME \t");
+    printf("PHONE NUMBER  \t");
+    printf("TYPE OF FRIENDS \t");
+    printf("NAME OF COMMON FRIENDS \t");
+    printf("NUMBER OF PLACES VISITED TOGETHER  \t\n");
+    for(i=0;i<n;i++){
+        printf("%d \t",i+1);
+        printf("%S \t",f[i].name);
+        printf("%s \t",f[i].pet_name);
+        printf("%lf \t",f[i].phone_number);
+        printf("%s \t",f[i].t.type_of_friends);
+        printf("%s \t",f[i].t.name_of_friends);
+        printf("%d \t\n",f[i].t.place_visited);
     }
-
-    float totalCost = 0;
-    float maxPrice = (products + 0)->price;
-    float minPrice = (products + 0)->price;
-    int maxIndex = 0;
-    int minIndex = 0;
-
-    for (int i = 1; i < n; i++) {
-        totalCost += (products + i)->price;
-        if ((products + i)->price > maxPrice) {
-            maxPrice = (products + i)->price;
-            maxIndex = i;
-        }
-        if ((products + i)->price < minPrice) {
-            minPrice = (products + i)->price;
-            minIndex = i;
-        }
-    }
-    totalCost += (products + 0)->price;  
-
-    printf("\nMost Expensive Product: %s, Product ID: %s, Price: %.2f\n",
-           (products + maxIndex)->name, (products + maxIndex)->id, (products + maxIndex)->price);
-    printf("Least Expensive Product: %s, Product ID: %s, Price: %.2f\n",
-           (products + minIndex)->name, (products + minIndex)->id, (products + minIndex)->price);
-
-    printf("\nTotal Cost of All Products: %.2f\n", totalCost);
-
-    free(products);
-
-    return 0;
 }
